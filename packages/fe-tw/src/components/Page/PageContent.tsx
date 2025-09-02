37f90d84e0061de3e1aec125ca3630800d590a3a
mykyta_yefruiev <efruev@gmail.com>
2025-09-02 12:04:43 +0200

    Remove a tone of unsused code
    Signed-off-by: mykyta_yefruiev <efruev@gmail.com>

--- packages/fe-tw/src/components/Page/PageContent.tsx
+++
@@ -1,5 +0,0 @@
-import { JSX } from "react";
-
-export const PageContent = ({ children }: { children: JSX.Element }) => {
-   return <div className="p-0 pl-0">{children}</div>;
-};
--- packages/fe-tw/src/components/Slices/index.tsx
+++ packages/fe-tw/src/components/Slices/index.tsx
@@ -1,3 +1,2 @@
 export { SlicesOverview } from "./SlicesOverview";
 export { SliceDetailPage } from "./SliceDetailPage";
-export { SliceItem } from "./SliceItem";
